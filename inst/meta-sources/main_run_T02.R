68b24c210bf8bb1a194418608193986dc2ba0f27
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-01-17 16:49:29 +0100

    Fix typo in updated name of main PGAS function

--- inst/meta-sources/main_run_T02.R
+++ inst/meta-sources/main_run_T02.R
@@ -18,8 +18,8 @@ model <- BNMPD::ModelBNMPD$new(path_to_project = pths_in$pth_project,
 # model$set_param_inits(pths_in$pth_params_true)
 
 pgas_model <- model$load_modeldata_runtime_pgas()
-out <- pgas_d(pgas_model, sim_type = "pmcmc", mod_type = "simulation",
-              settings_seed = list(seed_all_init = 123))
+out <- pgas(pgas_model, sim_type = "pmcmc", mod_type = "simulation",
+            settings_seed = list(seed_all_init = 123))
 model$save_pgas_model_out(out)
 out_all <- model$get_model_output()
 
