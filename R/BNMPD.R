38142e0ab31baf4a2414a6ee3a171a0961936414
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-06-06 19:46:41 +0200

    docs: minor update to package description

--- R/BNMPD.R
+++ R/BNMPD.R
@@ -1,12 +1,20 @@
-#' BNMPD: A package for modeling and estimation of Bayesian
-#' non-parametric multivariate panel data
+#' BNMPD: Learning of Bayesian non-parametric multivariate panel data models
 #'
 #' This package provides facilities for running a Particle Gibbs with Ancestor
 #' sampling procedure on various multivariate (non-parametric) Bayesian panel
 #' data models.
 #'
-#' @section foo functions:
-#' The foo functions ...
+#' The distributions of measurement models currently implemented are
+#'  * "Dirichlet"
+#'  * "Generalized Dirichlet"
+#'  * "Dirichlet Multinomial"
+#'  * "Generalized Dirichlet Multinomial"
+#'
+#' @section Measurement model details:
+#'     To be written.
+#'
+#' @section Specifications for regressors:
+#'     To be written.
 #'
 ## BNMPD namespace: start
 #' @useDynLib BNMPD, .registration = TRUE
