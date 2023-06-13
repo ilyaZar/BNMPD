bfd4bfd9ee6fd18cdb0d85d1c57ae6ea3efda534
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-06-13 14:21:08 +0200

    refactor: pgas templates for better use in simuls

--- inst/meta-sources/main_run_T02.R
+++ inst/meta-sources/main_run_T02.R
@@ -1,8 +1,6 @@
 library(parallel)
-library(snow)
 library(Rmpi)
 library(BNMPD)
-library(pmcmcDiagnostics)
 
 pth_model <- dirname(rstudioapi::getSourceEditorContext()$path)
 pths_in  <- BNMPD::get_paths_modelBNMPD_input(pth_model)
@@ -22,25 +20,3 @@ out <- pgas(pgas_model, sim_type = "pmcmc", mod_type = "simulation",
             settings_seed = list(seed_all_init = 123))
 model$save_pgas_model_out(out)
 out_all <- model$get_model_output()
-
-meta_labels_names <- model$get_par_label_names()
-set_plt <- list(plot_view = FALSE,
-                plot_ggp2 = FALSE,
-                plot_save = TRUE,
-                plot_save_all = TRUE,
-                plot_name = pths_ou$fnm_plots,
-                plot_path = pths_ou$pth_plots)
-set_tbl <- list(table_view = TRUE,
-                table_save = TRUE,
-                table_name = pths_ou$fnm_table,
-                table_path = pths_ou$pth_table,
-                table_prec = 8)
-set_urs <- list(ur_view = TRUE, ur_save = TRUE,
-                ur_name = "update_rates",
-                ur_path = pths_ou$pth_plots)
-
-out_diagnostics <- analyse_mcmc_convergence2(out_all,
-                                             model_meta = meta_labels_names,
-                                             settings_plots = set_plt,
-                                             settings_table = set_tbl,
-                                             settings_urs = set_urs)
