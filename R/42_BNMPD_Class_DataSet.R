8416a2d218850e43bc205d17a5df2fa0f8d3cb87
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-06-15 11:40:43 +0200

    refactor: make DataSet class lightweight from outside
    - remove redundant public member functions that provide features rarely needed
    - improve docs

--- R/42_BNMPD_Class_DataSet.R
+++ R/42_BNMPD_Class_DataSet.R
@@ -1,54 +1,22 @@
 #' R6 Class representing a data set
 #'
 #' @description Storage class for "raw" data set equipped with basic print and
-#'   view members for data, getters/setters for the path and the data itself.
-#'   For internal usage within [`ModelBNMPD`] only.
+#'   view functions. Usually for internal usage within [`ModelBNMPD`], but if
+#'   the data set directory has several files `update_dataset()` can be called
+#'   to chose among the files (rarely used feature ...).
 #'
-#' @details Data set stored within class is not writable and meant to be
-#'   regenerated (re-loaded) every time the class is initialized.
+#' @details Data set stored within class is not writable and regenerated
+#'   (re-loaded) every time the main [`ModelBNMPD`] class is initialized. If an
+#'   update is necessary, directly alter the data set file (`.csv`) in the model
+#'   directory `model/input/datasets`.
 DataSet <- R6::R6Class(classname = "DataSet",
                        class = FALSE,
                        cloneable = FALSE,
                        portable = FALSE,
                        private = list(
-                         pth_to_data = NULL,
-                         pth_to_dataset = NULL,
-                         dataset = NULL
-                       ),
-                       public = list(
-                         #' @description Class initializer
-                         #'
-                         #' @param path_to_data character string; path to data
-                         #'   passed internally via [`ModelBNMPD`] construction
-                         initialize = function(path_to_data) {
-                           private$pth_to_data <- path_to_data
-
-                           self$update_dataset(FALSE)
-                         },
-                         #' @description updates data set
-                         #' @details searches in data folder and loads the data
-                         #'   file; if several, the user can choose
-                         #' @param print_data logical; if `FALSE`, data is not
-                         #'   printed to screen otherwise data is printed as a
-                         #'   tibble
-                         update_dataset = function(print_data = TRUE) {
-                           data_chosen <- NULL
-                           data_chosen <- self$get_data(private$pth_to_data,
-                                                        print_data)
-                           private$dataset <- data_chosen
-                           invisible(self)
-                         },
-                         #' @description Reads data from specified path
-                         #' @details Prompts the user to choose data set if
-                         #'   there are several option, and prints data to
-                         #'   screen optionally.
-                         #' @param pth_tmp path to data set passed internally
-                         #'   (not for manual use)
-                         #' @param print_data logical; if `TRUE`, prints data
-                         #'   set to screen, and is typically setting by caller
-                         #'   (a method from [`ModelBNMPD`])
-                         #'
-                         #' @return returns a `data.frame`
+                         .pth_to_data = NULL,
+                         .pth_to_dataset = NULL,
+                         .dataset = NULL,
                          get_data = function(pth_tmp, print_data) {
                            datasets_all <- list.files(pth_tmp)
                            num_files <- length(datasets_all)
@@ -74,37 +42,55 @@ DataSet <- R6::R6Class(classname = "DataSet",
                            if(print_data) print(tibble::tibble(data_chosen))
                            message(crayon::green("Data successfully loaded!"))
                            return(data_chosen)
+                         }
+                       ),
+                       public = list(
+                         #' @description Class initializer; reads data from
+                         #'   specified path during instantiation
+                         #'
+                         #' @details Prompts the user to choose data set if
+                         #'   there are several options (files).
+                         #'
+                         #' @param path_to_data character string; path to data
+                         #'   directory passed internally via [`ModelBNMPD`]
+                         #'   instantiation
+                         initialize = function(path_to_data) {
+                           private$.pth_to_data <- path_to_data
+                           self$update_dataset(FALSE)
                          },
-                         #' @description Returns path to data set
-                         #' @return character
-                         get_pth_dataset = function() {
-                           cat(pth_to_dataset)
-                           return(private$pth_to_dataset)
-                         },
-                         #' @description Returns path to data directory
-                         #' @return character
-                         get_pth_data = function() {
-                           cat(pth_to_data)
-                           return(private$pth_to_data)
+                         #' @description Updates data set i.e. forces the main
+                         #'   class to-regenerate the internal data set field.
+                         #' @details Useful if another data set is put into the
+                         #'   data set directory and the model class shall not
+                         #'   be destroyed. Looks in data directory and user
+                         #'   can choose which `.csv`-file to load.
+                         #' @param print_data logical; if `FALSE`, data is not
+                         #'   printed to screen otherwise data is printed as a
+                         #'   tibble
+                         update_dataset = function(print_data = TRUE) {
+                           private$.dataset <- private$get_data(
+                             private$.pth_to_data,
+                             print_data)
+                           invisible(self)
                          },
                          #' @description View data set
                          #' @return side effect; call to `View()`
                          view_data_set = function() {
-                           utils::View(private$dataset)
+                           utils::View(private$.dataset)
                          },
                          #' @description Prints data set
                          #'
-                         #' @param ... additional arguments passed furhter to
-                         #'   \code{print()}
-                         #' @return side-effect; prints to screen
+                         #' @param ... additional arguments passed further to
+                         #'   `print()`
+                         #' @return side-effect; prints to console
                          print_data_set = function(...) {
-                           print(tibble::tibble(private$dataset), ...)
+                           print(tibble::tibble(private$.dataset), ...)
                          },
-                         #' @description Returns data set as data.frame or
-                         #'   tibble as set during initialization
+                         #' @description Returns data set as `data.frame()` or
+                         #'   `tibble()` as set during initialization
                          #' @return data frame or tibble of data
                          get_data_set = function() {
-                           private$dataset
+                           private$.dataset
                          }
                        )
 )
