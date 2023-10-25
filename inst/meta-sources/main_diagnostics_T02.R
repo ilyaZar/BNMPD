4a3d879beff4c2edfceec26f6219b2736d0cbc0b
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-10-25 12:28:50 +0200

    style: improve formatting of meta files

--- inst/meta-sources/main_diagnostics_T02.R
+++ inst/meta-sources/main_diagnostics_T02.R
@@ -6,12 +6,12 @@ pth_mod <- get_path_to_model()
 pths_in <- get_paths_modelBNMPD_input(pth_mod)
 pths_ou <- get_paths_modelBNMPD_results(pth_mod)
 
-model <- BNMPD::ModelBNMPD$new(path_to_project = pths_in$pth_project,
-                               path_to_states_init = NULL,
-                               path_to_states_true = NULL,
-                               path_to_params_init = NULL,
-                               path_to_params_true = NULL,
-                               AUTO_INIT = FALSE)
+model <-ModelBNMPD$new(path_to_project = pths_in$pth_project,
+                       path_to_states_init = NULL,
+                       path_to_states_true = NULL,
+                       path_to_params_init = NULL,
+                       path_to_params_true = NULL,
+                       AUTO_INIT = FALSE)
 
 out_all <- model$get_model_output()
 # fn <- paste0("out_", basename(pth_model))
@@ -21,7 +21,7 @@ model$save_pgas_model_out(out_all, out_name = fn, AUTO_INIT = FALSE)
 # meta_labels_names <- model$get_par_label_names()
 # set_plt <- list(plot_view = FALSE,
 #                 plot_ggp2 = FALSE,
-#                 plot_save = TRUE,
+#                 plot_save = FALSE,
 #                 plot_save_all = TRUE,
 #                 plot_name = pths_ou$fnm_plots,
 #                 plot_path = pths_ou$pth_plots)
@@ -33,7 +33,7 @@ model$save_pgas_model_out(out_all, out_name = fn, AUTO_INIT = FALSE)
 # set_urs <- list(ur_view = TRUE, ur_save = TRUE,
 #                 ur_name = "update_rates",
 #                 ur_path = pths_ou$pth_plots)
-# set_mcmc = list(burn = 10000, thin = NULL,
+# set_mcmc = list(burn = 5000, thin = NULL,
 #                 ki_prob = 0.9,
 #                 q_probs = c(0.025, 0.25, 0.5, 0.75, 0.975),
 #                 compute_ess = TRUE,
@@ -43,5 +43,6 @@ model$save_pgas_model_out(out_all, out_name = fn, AUTO_INIT = FALSE)
 #                                              model_meta = meta_labels_names,
 #                                              settings_mcmc = set_mcmc,
 #                                              settings_plots = set_plt,
-#                                              settings_table = set_tbl,
-#                                              settings_urs = set_urs)
+#                                              settings_table = set_tbl)
+#
+# out_diagnostics_states <- analyse_states_convergence(out_all, set_urs)
--- inst/meta-sources/main_run_T02.R
+++ inst/meta-sources/main_run_T02.R
@@ -6,11 +6,11 @@ pth_mod <- get_path_to_model()
 pths_in <- get_paths_modelBNMPD_input(pth_mod)
 pths_ou <- get_paths_modelBNMPD_results(pth_mod)
 
-model <- BNMPD::ModelBNMPD$new(path_to_project = pths_in$pth_project,
-                               path_to_states_init = pths_in$pth_states_true,
-                               path_to_states_true = pths_in$pth_states_true,
-                               path_to_params_init = pths_in$pth_params_true,
-                               path_to_params_true = pths_in$pth_params_true)
+model <- ModelBNMPD$new(path_to_project = pths_in$pth_project,
+                        path_to_states_init = pths_in$pth_states_true,
+                        path_to_states_true = pths_in$pth_states_true,
+                        path_to_params_init = pths_in$pth_params_true,
+                        path_to_params_true = pths_in$pth_params_true)
 
 # model$set_param_inits(pths_in$pth_params_defl)
 # model$set_param_inits(pths_in$pth_params_true)
@@ -19,7 +19,9 @@ model <- BNMPD::ModelBNMPD$new(path_to_project = pths_in$pth_project,
 ################################## LOCAL CGS ###################################
 ################################################################################
 pgas_model <- model$load_modeldata_runtime_pgas()
-out <- pgas(pgas_model, sim_type = "pmcmc", mod_type = "simulation",
+out <- pgas(pgas_model,
+            sim_type = "pmcmc",
+            mod_type = "simulation",
             settings_seed = list(seed_all_init = 123))
 model$save_pgas_model_out(out)
 # out_all <- model$get_model_output()
