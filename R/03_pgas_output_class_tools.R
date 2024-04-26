8b9302d356863bdb6bbdfc2d7cad3f87ee915dd7
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-04-26 18:36:05 +0200

    feat: add support for proper marginal effect computation and CI bands/fit

--- R/03_pgas_output_class_tools.R
+++ R/03_pgas_output_class_tools.R
@@ -1,3 +1,276 @@
+#' Compute marginal effects of regressors on a grid
+#'
+#' @param out an object of class [outBNMPD][BNMPD::new_outBNMPD()] (as
+#'   returned e.g. via [BNMPD::pgas()])
+#' @param data_set_raw a data set of energy shares; see examples code on how
+#'   such a data set can be produced; if \code{model} is an object of type
+#'   [BNMPD::ModelBNMPD], then \code{model$get_data_set_raw()} can be used
+#' @param data_set_internal_regs a data set of internal regressors; if
+#'   \code{model} is an object of type [BNMPD::ModelBNMPD],
+#'   \code{model$get_data_internal()} can be used
+#' @param settings_list a list of settings with fields `burn`, `thin`and
+#'   `KI_probs` to specify the burn-in period, thinning and the quantiles for
+#'   the confidence intervals of the fitted values
+#'
+#' @return an array of dimension `TT x DD x NN x 3` where the first dimension is
+#'   the time series length, the second dimension is the number of multivariate
+#'   components (including the zero component), the third dimension is the
+#'   number of cross-sectional units, and the fourth dimension is the number of
+#'   measures (mean, lower and upper bounds of the confidence interval)
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' # This is the first part of a `main_diagnostics.R` script so posterior fit
+#' # computation can be invoked from a given `out_all` object
+#' pth_mod <- get_path_to_model()
+#' pths_in <- get_paths_modelBNMPD_input(pth_mod)
+#' pths_ou <- get_paths_modelBNMPD_results(pth_mod)
+#'
+#' model <- ModelBNMPD$new(path_to_project = pths_in$pth_project,
+#'                         path_to_states_init = NULL,
+#'                         path_to_states_true = NULL,
+#'                         path_to_params_init = NULL,
+#'                         path_to_params_true = NULL,
+#'                         AUTO_INIT = FALSE)
+#' out_all <- model$get_model_output()
+#' data_posterior_fit <- BNMPD:::compute_outBNMPD_fit(
+#'   out_all, settings_list = list(
+#'     burn = 2500,
+#'     thin = 10,
+#'     KI_probs = c(0.025, 0.975)
+#'   )
+#' )
+#' }
+compute_outBNMPD_mes <- function(
+    out,
+    data_set_raw,
+    data_set_internal_regs,
+    settings_list = list(
+      setup_mcmc = list(
+        burn = NULL,
+        thin = NULL,
+        KI_probs = c(0.025, 0.975)
+      ),
+      setup_marginal_effects = list(
+        grid_length = 30,
+        mcmc_comp_match = list(
+          d_1 = "k_1",
+          d_2 = "k_1",
+          d_3 = "k_1",
+          d_4 = "k_1",
+          d_5 = "k_1"
+        ),
+        regs_comp_match = list(
+          d_1 = "gdp_grw",
+          d_2 = "gdp_grw",
+          d_3 = "gdp_grw",
+          d_4 = "gdp_grw",
+          d_5 = "gdp_grw"
+        )
+      )
+    )
+) {
+  check_class_outBNMPD(out)
+  mod_type_obs <- get_mod_type_obs(out)
+  SPECIAL_DIST <- check_special_dist_quick(mod_type_obs)
+
+  out_x <- burn_and_thin(
+    out$x, dim_mcmc = 3,
+    burnin = settings_list$setup_mcmc$burn,
+    thin = settings_list$setup_mcmc$thin)
+  bet_z <- burn_and_thin(
+    out$bet_z, dim_mcmc = 2,
+    burnin = settings_list$setup_mcmc$burn,
+    thin = settings_list$setup_mcmc$thin)
+  bet_u <- burn_and_thin(
+    out$bet_u, dim_mcmc = 2,
+    burnin = settings_list$setup_mcmc$burn,
+    thin = settings_list$setup_mcmc$thin)
+  phi_x <- burn_and_thin(
+    out$phi_x, dim_mcmc = 2,
+    burnin = settings_list$setup_mcmc$burn,
+    thin = settings_list$setup_mcmc$thin)
+
+  # parameters burned and thinned
+  out_buind <- list(out_x = out_x, bet_z = bet_z, bet_u = bet_u, phi_x = phi_x)
+  TT <- dim(out_x)[1]
+  if (SPECIAL_DIST) {
+    DD <- dim(out_x)[2] / 2 + 1
+  } else {
+    DD <- dim(out_x)[2]
+  }
+  MM <- dim(out_x)[3]
+  NN <- dim(out_x)[4]
+  # 3 measures mean, CI lower, and CI upper bounds
+  NM <- 3
+  # Grid length is passed via settings object
+  GG <- settings_list$setup_marginal_effects$grid_length
+  out_x_fit <- generate_out_x_fit(out_buind,
+                                  data_set_internal_regs,
+                                  TT, DD, MM, NN,
+                                  LOGARITHM = TRUE)
+  out_all <- array(0, dim = c(TT, DD, NN, NM))
+  for (nn in seq_len(NN)) {
+    tmp_list <- switch(
+      mod_type_obs,
+      "GEN_DIRICHLET" =  get_1st_moment_GD_matrix(
+        out_x_fit[, , , nn], TT, DD, MM, settings_list = settings_list$setup_mcmc),
+      "DIRICHLET" = get_1st_moment_D_matrix(
+        out_x_fit[, , , nn], TT, DD, MM, settings_list = settings_list$setup_mcmc)
+    )
+    out_all[, , nn, 1] <- tmp_list$out_means
+    out_all[, , nn, c(2, 3)] <- tmp_list$out_KI
+    progress_any(nn, NN)
+  }
+  out_dimnames <- list(dimnames(tmp_list$out_KI)[[1]],
+                       dimnames(tmp_list$out_KI)[[2]],
+                       paste0("NN_",
+                              formatC(seq_len(NN),
+                                      width = nchar(NN),
+                                      format = "d",
+                                      flag = "0")),
+                       c("mean", dimnames(tmp_list$out_KI)[[3]]))
+  dimnames(out_all) <- out_dimnames
+  return(out_all)
+}
+generate_out_x_fit <- function(out, regs, TT, DD, MM, NN, LOGARITHM) {
+  out_x <- out$out_x
+  bet_z <- out$bet_z
+  bet_u <- out$bet_u
+  phi_x <- out$phi_x
+
+  Z <- regs$Z
+  U <- regs$U
+  out_x_fit <- array(0, dim = c(TT, DD, MM, NN))
+  dimnames(out_x_fit) <- dimnames(out$out_x)
+  id_regs_z <- get_dim_regs(regs = Z, DD = DD)
+  id_regs_u <- get_dim_regs(regs = U, DD = DD)
+  # remove first observation
+  TT_SEQ <- seq_len(TT)[-c(1)]
+  for (mm in seq_len(MM)) {
+    for (nn in seq_len(NN)) {
+      for (dd in seq_len(DD)) {
+        out_x_fit[1, dd, mm, nn] <- out_x[1, dd, mm, nn]
+      }
+    }
+  }
+  for (nn in seq_len(NN)) {
+    id_zero <- get_zero_component_id(
+      out_x[, , , nn],
+      DD,
+      type = "STANDARD",
+      z_val_def = 0
+    )
+    for (tt in TT_SEQ) {
+      for (mm in seq_len(MM)) {
+        for (dd in seq_len(DD)) {
+          if (dd %in% id_zero) {
+            out_x_fit[tt, dd, mm, nn] <- 0
+          } else {
+          Z_x_beta_z <- sum(Z[tt, id_regs_z[[dd]], nn] * bet_z[id_regs_z[[dd]], mm])
+          U_x_beta_u <- sum(U[tt, id_regs_u[[dd]], nn] * bet_u[id_regs_u[[dd]], mm, nn])
+          phi_x_out_x <- sum(phi_x[dd, mm] * out_x[tt - 1, dd, mm, nn])
+          out_x_fit[tt, dd, mm, nn] <- phi_x_out_x + Z_x_beta_z + U_x_beta_u
+          }
+        }
+      }
+    }
+  }
+  if (isFALSE(LOGARITHM)) out_x_fit <- exp(out_x_fit)
+  return(out_x_fit)
+}
+get_dim_regs <- function(regs, DD) {
+  DD_regex <- formatC(seq_len(DD), width = 2, format = "d", flag = "0")
+  out_id_list <- vector("list", DD)
+  names_to_search <- dimnames(regs)[[2]]
+  for (dd in seq_len(DD)) {
+    out_id_list[[dd]] <- grep(DD_regex[dd], names_to_search)
+  }
+  return(out_id_list)
+}
+get_1st_moment_D_matrix <- function(
+    x, TT, DD, MM, settings_list = list(KI_probs = c(0.025, 0.975))) {
+  x_exp <- exp(x)
+
+  TT  <- dim(x_exp)[1]
+  DD  <- dim(x_exp)[2]
+  MM  <- dim(x_exp)[3]
+
+  id_zeros <- get_zero_component_id(x_exp, DD, type = "STANDARD")
+  out_cnt_all <- array(0, dim = c(TT, DD, MM))
+  for (tt in seq_len(TT)) {
+    for (dd in seq_len(DD)) {
+      if (dd %in% id_zeros) {
+        out_cnt_all[, dd, ] <- 0
+      } else {
+        for (mm in seq_len(MM)) {
+          out_cnt_all[tt, dd, mm] <- compute_1st_moment_D(
+            alpha = x_exp[tt, , mm],
+            num_c = dd
+          )
+        }
+      }
+    }
+  }
+
+  out_means <- apply(out_cnt_all, c(1, 2), mean)
+  out_KI <- apply(out_cnt_all, c(1, 2), function(x) {
+    quantile(x, probs = settings_list$KI_probs)
+  })
+  out_KI <- aperm(out_KI, c(2, 3, 1))
+  dimnames(out_KI) <- list(dimnames(x_exp)[[1]],
+                           paste0("D_0", seq_len(DD)),
+                           paste0(c("KI_low_", "KI_upp_"),
+                                  settings_list$KI_probs * 100))
+  return(list(out_means = out_means, out_KI = out_KI))
+}
+get_1st_moment_GD_matrix <- function(
+    x, TT, DD, MM, settings_list = list(KI_probs = c(0.025, 0.975))
+) {
+  x_exp <- exp(x)
+  a_par <- x_exp[, get_id_param_gen_dir(colnames(x_exp), "DA"), ]
+  b_par <- x_exp[, get_id_param_gen_dir(colnames(x_exp), "DB"), ]
+
+  TT  <- dim(a_par)[1]
+  DD  <- dim(a_par)[2] + 1
+  MM  <- dim(a_par)[3]
+
+  id_zeros_a <- get_zero_component_id(a_par, DD, type = "GENERALIZED")
+  id_zeros_b <- get_zero_component_id(b_par, DD, type = "GENERALIZED")
+  stopifnot(`Unequal zero matches.` = all.equal(id_zeros_a, id_zeros_b))
+
+  id_zeros <- id_zeros_a
+  out_cnt_all <- array(0, dim = c(TT, DD, MM))
+  for (tt in seq_len(TT)) {
+    for (dd in seq_len(DD - 1)) {
+      if (dd %in% id_zeros) {
+        out_cnt_all[, dd, ] <- 0
+      } else {
+        for (mm in seq_len(MM)) {
+          out_cnt_all[tt, dd, mm] <- compute_1st_moment_GD(
+            a = a_par[tt, , mm],
+            b = b_par[tt, , mm],
+            num_c = dd
+          )
+        }
+      }
+    }
+  }
+  for (mm in seq_len(MM)) {
+    out_cnt_all[, DD, mm] <- 1 - rowSums(out_cnt_all[, 1:(DD - 1), mm])
+  }
+  out_means <- apply(out_cnt_all, c(1, 2), mean)
+  out_KI <- apply(out_cnt_all, c(1, 2), function(x) {
+    quantile(x, probs = settings_list$KI_probs)
+  })
+  out_KI <- aperm(out_KI, c(2, 3, 1))
+  dimnames(out_KI) <- list(dimnames(a_par)[[1]],
+                           paste0("D_0", seq_len(DD)),
+                           paste0(c("KI_low_", "KI_upp_"),
+                                  settings_list$KI_probs * 100))
+  return(list(out_means = out_means, out_KI = out_KI))
+}
 #' Compute fitted values from the output of the BNMPD model
 #'
 #' @param out an object of class [outBNMPD][BNMPD::new_outBNMPD()] (as
