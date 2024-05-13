f97c1f953b388c9af8a03c627c2a6e817d59c302
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-05-13 17:26:46 +0200

    style: add newline and spacing

--- R/42_BNMPD_helper.R
+++ R/42_BNMPD_helper.R
@@ -9,14 +9,14 @@ copy_envs <- function(to_env = NULL, ...) {
 }
 copy_env = function(to_env = NULL, from_env) {
   if (is.null(to_env)) to_env <- new.env()
-  for(n in ls(from_env, all.names = TRUE)) {
+  for (n in ls(from_env, all.names = TRUE)) {
     assign(n, get(n, from_env), to_env)
   }
   invisible(to_env)
 }
 copy_env_to_parent = function(from_env) {
   to_env <- parent.frame()
-  for(n in ls(from_env, all.names = TRUE)) {
+  for (n in ls(from_env, all.names = TRUE)) {
     assign(n, get(n, from_env), to_env)
   }
   invisible(to_env)
