ef7fbb941d608bd0db32480bcd28736b6a9eeb9b
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-12-29 11:56:45 +0100

    fix: warning due to type missmatch

--- src/100_vscode_debuger_tests.cpp
+++ src/100_vscode_debuger_tests.cpp
@@ -1,4 +1,4 @@
-#include <RcppArmadillo.h>
+#include "RcppArmadillo.h"
 
 //' Simple sum
 //' @param x a numeric vector
@@ -6,7 +6,7 @@
 // [[Rcpp::export]]
 double calc_sum(arma::vec x) {
   double sum = 0;
-  for (int i = 0; i < x.size(); ++i) {
+  for (arma::uword i = 0; i < x.size(); ++i) {
     std::cout << i << std::endl;
     sum += x[i];
   }
